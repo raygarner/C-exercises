227e748b2f526483b74ac01612e7834c5d062144
raygarner <ray@perfectcast.co.uk>
2018-10-14 12:41:07 +0100

    1-10

--- 1/9.c
+++ 1/9.c
@@ -1,3 +1,5 @@
+//write a program to copy its input to its output but replace blocks of spaces with a single space
+//write a program to copy its input with its output but replace tabs with \t, backspaces with \b and backslashes with two backslashes
 
 #include <stdio.h>
 #include <stdbool.h>
@@ -9,22 +11,35 @@ int main()
 
     while ((c = getchar()) != EOF) {
         
-        if (c == ' ') {
+    
+        
+        switch (c){
+        case ' ':
             putchar(' ');
             while ((c = getchar()) == ' ');
             putchar(c);
-        } else if (c == '\t'){
-            putchar('\\\t');
-        } else if (c == '\b'){
-            putchar('\\\b');
-        } else if (c == '\\'){
-            putchar('\\');
-            putchar('\\');
-        }else{
-            putchar(c);
-        }
-    
+            break;
         
+        case '\t':
+            putchar('\\\t');
+            break;
+        
+        case '\b':
+            putchar('\\\b');
+            break;
+        
+        case '\\':
+            putchar('\\');
+            putchar('\\');
+            break;
+
+        default: 
+            putchar(c);
+            //break;
+        
+
+        }
+
 
     }
 }
